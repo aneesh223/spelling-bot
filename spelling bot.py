ea78b1373a7f908827cc4d4330eeff2cec7a18d0
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2021-10-15 12:48:35 -0400

    Commit 1

--- spelling bot.py
+++ spelling bot.py
@@ -1,23 +1,25 @@
 import random, pyttsx3
-from list_maker import parsedlst as wordlist
+from list_maker import parsedlst
 
 def bot(list_):
     engine = pyttsx3.init()
-    print("welcome to the spelling bot! type /quit to quit")
-    while True:  
+    voice = engine.getProperty('voices')[0]
+    engine.setProperty('voice',voice.id)
+    print("welcome to the spelling bot! type /q to stop the bot, or /r to repeat the word")
+    while True:
         word = random.choice(list_)
-        while True:    
+        while True:
             engine.say (f"Your word is: {word}")
             engine.runAndWait()
-            input_ = input("Answer: ")
+            input_ = input("")
             if input_ == word:
                 print("Correct!")
                 engine.say ("Correct")
                 engine.runAndWait()
                 break
-            elif input_ == "/quit":
+            elif input_ == "/q":
                 quit()
-            elif input_ == "/repeat":
+            elif input_ == "/r":
                 pass
             else:
                 spelling = " ".join(word)
@@ -26,8 +28,8 @@ def bot(list_):
                 engine.say(spelling)
                 engine.runAndWait()
                 break
-        
+
 
 
 if __name__ == '__main__':
-    bot(wordlist)
+    bot(parsedlst)
---
+++ ui.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=150, width=300)
+canvas.pack()
+
+frame = tk.Frame(root, bg='#6B5A00')
+frame.place(relheight=1, relwidth=1)
+
+wrd = tk.Entry(frame, width=50, bg='#6B5A00', fg='#FFFFFF')
+wrd.pack()
+
+submit = tk.Button(frame, text='Send', padx=10,
+                 pady=5, fg='#FFFFFF', bg='#6B5A00',
+                 command=lambda: )
+submit.pack()
+
+root.mainloop()
